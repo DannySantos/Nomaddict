a12d208d72fd165a55b32000c2110bd704b00eb1
Danny Santos <danny@ethicalswitch.com>
2017-04-16 12:28:56 +0100

    User name bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  before_create :add_name
+  
+  def add_name
+    self.name = "Anonymous" if self.name.blank?
+  end
 end
