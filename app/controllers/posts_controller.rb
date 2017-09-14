2c8bb15d540f4bf938af4c4d86bc60eadee6ef93
Danny Santos <danny@ethicalswitch.com>
2017-09-14 14:34:03 +0100

    Nav style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  layout "blog"
 
   def index
     @posts = Post.all
